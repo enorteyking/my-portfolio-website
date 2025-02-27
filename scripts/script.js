2605500512e1bda51c78c38744b2763f03ee64ac
Emmanuel Nortey-King <nkingemma@gmail.com>
2025-02-27 08:04:36 +0000

    additional updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,26 @@
-// function countUp(target, duration) {
-//     let counter = document.getElementById("counter");
-//     let start = 0;
-//     let increment = target / (duration / 10); // Controls speed
-//     let interval = setInterval(() => {
-//         start += increment;
-//         if (start >= target) {
-//             counter.innerText = target; // Ensure exact target number
-//             clearInterval(interval);
-//         } else {
-//             counter.innerText = Math.floor(start);
-//         }
-//     }, 10);
-// }
+function countUp(counter) {
+    let target = parseInt(counter.getAttribute("data-target"));
+    let start = 0;
+    let duration = 2000; // 2 seconds
+    let increment = target / (duration / 10);
 
-// countUp(100, 2000); // Count to 500 in 2 seconds
-
-
-function countUpAll() {
-    const counters = document.querySelectorAll(".counter");
-
-    counters.forEach(counter => {
-        let target = parseInt(counter.getAttribute("data-target"));
-        let start = 0;
-        let duration = 2000; // 2 seconds
-        let increment = target / (duration / 10);
-
-        let interval = setInterval(() => {
-            start += increment;
-            if (start >= target) {
-                counter.innerText = target; // Ensure exact final number
-                clearInterval(interval);
-            } else {
-                counter.innerText = Math.floor(start);
-            }
-        }, 10);
-    });
+    let interval = setInterval(() => {
+        start += increment;
+        if (start >= target) {
+            counter.innerText = target; // Ensure exact final number
+            clearInterval(interval);
+        } else {
+            counter.innerText = Math.floor(start);
+        }
+    }, 10);
 }
 
-countUpAll(); // Run the counter function for all numbers
+document.querySelectorAll(".counter-box").forEach(box => {
+    box.addEventListener("mouseover", function() {
+        let counter = this.querySelector(".counter");
+        counter.innerText = "0"; // Reset the counter before animating
+        countUp(counter);
+    });
+});
+
+countUp(); // Run the counter function for all numbers
